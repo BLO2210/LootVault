99fa43e9f335c84260377d434cc95afd45647f33
BLO2210 <brandonmichael2210@gmail.com>
2023-05-18 11:13:28 -0400

    Created Server Repo

--- Server/schemas/movieslist.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const MoviesListSchema = new mongoose.Schema({
-    poster: { type: String, required: true},
-    title: { type: String, required: true}
-  })
-
-  const MoviesList = mongoose.model("MoviesList", MoviesListSchema);
-  module.exports = MoviesList;
